e13c16b9a191192ed7137a8a931d8a593dbaad70
urltapas <ryan.bromati@hotmail.com>
2024-06-30 18:16:18 -0300

    feat: add onLogin sumit and names on tags

--- src/app/components/login-board/login-board.component.ts
+++ src/app/components/login-board/login-board.component.ts
@@ -1,12 +1,31 @@
+import { AuthService } from './../../auth/auth.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-board',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, HttpClientModule],
   templateUrl: './login-board.component.html',
 })
+
 export class LoginBoardComponent {
-  const email: 
+  email:string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+  
+  onLogin(): void {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        // Redirecionar após o login bem-sucedido
+        this.router.navigate(['/dashboard']); // Certifique-se de que o caminho está correto
+      },
+      error: (err) => {
+        console.error('Login failed', err);
+      }
+    });
+  }
 }
--- src/app/components/register-board/register-board.component.ts
+++ src/app/components/register-board/register-board.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register-board',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register-board.component.html',
 })
-export class RegisterBoardComponent {
 
+export class RegisterBoardComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
 }
